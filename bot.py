f5e0dee22eed2c68e56ea7455850bb56e26fa2a9
Migue <68868522+miguemorales@users.noreply.github.com>
2021-09-14 19:15:19 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram.ext import Updater, CommandHandler 
+
+def start(update, context):
+  update.message.reply_text('Hola humano')
+  
+  
+if __name__ == '__main__':
+  updater = Updater(token='1990363639:AAEhkQXkwoTTD6wHn_e0nwWoP0MdaEZN94A', use_context = True)
+  dp = updater.dispatcher
+  
+  # handlers
+  dp.add_handler(CommandHandler('start', start))
+  
+  updater.start_polling()
+  updater.idle()    # aqui se queda el bot en espera hasta que se le de informacion
+  
