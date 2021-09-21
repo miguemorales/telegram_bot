46425c074a0dbc6c456507370112f2bc219efb8b
Migue <68868522+miguemorales@users.noreply.github.com>
2021-09-21 19:26:04 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -12,9 +12,9 @@ import telegram_send  # pip install telegram-send
 
 def BTC(driver):
         btcname = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[3]/div/a').text
-        btcprice = driverfind_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a').text
-        btc24h =  driverfind_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[5]/span').text
-        btc7d = driverfind_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[6]/span').text
+        btcprice = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a').text
+        btc24h =  driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[5]/span').text
+        btc7d = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[6]/span').text
         print(btcname,' precio:', btcprice, ' 24h:',btc24h,' 7d:',btc7d)
 
 
