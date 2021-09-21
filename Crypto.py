617f005824960169479e57b6193a65f2479b2690
Migue <68868522+miguemorales@users.noreply.github.com>
2021-09-21 19:41:53 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -14,8 +14,9 @@ def BTC(driver):
         btcname = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[3]/div/a').text
         btcprice = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a').text
         btc24h =  driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[5]/span').text
+        btc24hS = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[5]/span/span').text
         btc7d = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[6]/span').text
-        print(btcname,' precio:', btcprice, ' 24h:',btc24h,' 7d:',btc7d)
+        print(btcname,' precio:', btcprice, ' 24h:',btc24h,'signo:',btc24hS,' 7d:',btc7d)
 
 
 if __name__ == '__main__':
