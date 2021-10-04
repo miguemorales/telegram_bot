b4d38cac20b0818b211040e64edcf72a6c530285
Migue <68868522+miguemorales@users.noreply.github.com>
2021-10-04 19:51:40 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -15,7 +15,7 @@ def BTC(driver):
         btcprice = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a').text
         btc24h =  driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[5]/span').text
         btc7d = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[6]/span').text
-         print(btcname,'-> precio:', btcprice, ' %24h:',btc24h,' %7d:',btc7d)
+        print(btcname,'-> precio:', btcprice, ' %24h:',btc24h,' %7d:',btc7d)
         
         return btcprice, btc24h, btc7d
 
