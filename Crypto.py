f511c173e8af845ac616c8750ca6677dc45c4398
Migue <68868522+miguemorales@users.noreply.github.com>
2021-10-04 19:42:34 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -11,25 +11,25 @@ import telegram_send  # pip install telegram-send
 
 
 def BTC(driver):
-        name = driver.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[3]/div/a/div/div/p').text
-        btcname = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[3]/div/a').text
+        btcname = driver.find_element_by_xpath('/html/body/div/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[3]/div/a/div/div/p').text
         btcprice = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[4]/div/a').text
         btc24h =  driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[5]/span').text
         btc7d = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr[1]/td[6]/span').text
-        print(btcname,' precio:', btcprice, ' 24h:',btc24h,' 7d:',btc7d,"nombre:", name)
+        # print(btcname,'-> precio:', btcprice, ' %24h:',btc24h,' %7d:',btc7d)
         
         return btcprice, btc24h, btc7d
 
 
 if __name__ == '__main__':
 
+    Criptos = ['Bitcoin','Ethereum','Cardano','Polkadot']
     driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
     driver.get("https://coinmarketcap.com/")
 
     prevbtcprice, prevbtc24h, prevbtc7d = BTC(driver)
-    newbtcprice = 0
-    '''while(newbtcprice == prevbtcprice):
-        time.sleep(5)
+    newbtcprice = prevbtcprice
+    while(newbtcprice == prevbtcprice):
+        time.sleep(2)
         newbtcprice, newbtc24h, newbtc7d = BTC(driver)
         
     if (newbtcprice < prevbtcprice):
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         if(newbtc7d > prevbtc7d):
                 newbtc7d = -newbtc7d
                 
-    print('BTC-> precio:', btcprice, ' 24h:',btc24h,' 7d:',btc7d)'''
+    print('BTC-> precio:', btcprice, ' 24h:',btc24h,' 7d:',btc7d)
         
         
 
