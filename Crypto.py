523b647b473091e5bb9f844ab12a267f5e897784
Migue <68868522+miguemorales@users.noreply.github.com>
2021-10-05 21:29:22 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -49,9 +49,10 @@ if __name__ == '__main__':
     data1 = data0.copy()
     while (done < len(criptos)):
         time.sleep(2)
-        driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+        driver = webdriver.Chrome('chromedriver',options=chrome_options)
         driver.get("https://coinmarketcap.com/")
         for cripto in criptos:
+                print(cripto,': ','Entro, precio anterior: ',data0[cripto][1],'precio nuevo: ',Precios(driver,data0[cripto][0])[0])
                 if (data0[cripto][1] != Precios(driver,data0[cripto][0])[0] and cripto not in Cdone):
                         print('Entro, precio anterior: ',data0[cripto][1],'precio nuevo: ',Precios(driver,data0[cripto][0])[0], 'criptos hechas: ',Cdone)
                         done = done +1
