0e1130b35eaa8fe3fa59f324af2c4f31bcea52db
Migue <68868522+miguemorales@users.noreply.github.com>
2021-10-18 20:43:21 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -11,7 +11,6 @@ import telegram_send  # pip install telegram-send
 
 
 def Precios(driver,i):
-        print('hola')
         criptoname = driver.find_element_by_xpath('/html/body/div/div/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr['+str(i)+']/td[3]/div/a/div/div/p').text
         criptoprice = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr['+str(i)+']/td[4]/div/a').text
         cripto24h =  driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[2]/div/div[1]/div[5]/table/tbody/tr['+str(i)+']/td[5]/span').text
@@ -48,17 +47,20 @@ if __name__ == '__main__':
     done = 0
     Cdone = []
     data1 = data0.copy()
-    driver1 = webdriver.Chrome('chromedriver',options=chrome_options)
-    driver1.get("https://coinmarketcap.com/")
+    #driver1 = webdriver.Chrome('chromedriver',options=chrome_options)
+    #driver1.get("https://coinmarketcap.com/")
     while (done < len(criptos)):
         time.sleep(2)
-        driver1.refresh()
+        driver1 = webdriver.Chrome('chromedriver',options=chrome_options)
+        driver1.get("https://coinmarketcap.com/")
+        #driver1.refresh()
         print(Precios(driver1,data0['Bitcoin'][0])[0])
         for cripto in criptos:
                 if (data0[cripto][1] != Precios(driver1,data0[cripto][0])[0] and cripto not in Cdone):
                         #print('Entro, precio anterior: ',data0[cripto][1],'precio nuevo: ',Precios(driver1,data0[cripto][0])[0], 'criptos hechas: ',Cdone)
                         done = done +1
                         Cdone.append(cripto)
+        driver1.close()
     #once I know all i have old and new data, i compare
     data1 = data0.copy()                 
     for cripto in criptos:
